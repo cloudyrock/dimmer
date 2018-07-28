ecdd27bb169375e027f3d2817b83cf46bc91d54b
diepppa <aperezdieppa@gmail.com>
2018-07-28 16:03:19 +0100

    WIP

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 mvn --batch-mode \
-    -Dtag=dimmer-1.4.5 \
-    -Dproject.rel.com.github.cloudyrock.dimmer:dimmer-core2=1.4.5 \
-    -Dproject.dev.com.github.cloudyrock.dimmer:dimmer-core2=1.4.6 \
+    -Dtag=dimmer-1.4.6 \
+    -Dproject.rel.com.github.cloudyrock.dimmer:dimmer-core2=1.4.6 \
+    -Dproject.dev.com.github.cloudyrock.dimmer:dimmer-core2=1.4.7-SNAPSHOT \
     release:prepare
