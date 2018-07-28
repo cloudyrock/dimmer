c6754475c6944948684e54dddcb3b8dda05a3dd1
diepppa <aperezdieppa@gmail.com>
2018-07-28 16:39:19 +0100

    WIP

--- release.sh
+++ release.sh
@@ -1,7 +1,15 @@
 #! /bin/sh
 
-mvn --batch-mode \
-    -Dtag=dimmer-1.4.7 \
-    -Dproject.rel.com.github.cloudyrock.dimmer:dimmer-core2=1.4.7 \
-    -Dproject.dev.com.github.cloudyrock.dimmer:dimmer-core2=1.4.8-SNAPSHOT \
-    release:prepare
+#$1 release version
+#$2 dev version
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "var is blank";
+else
+    mvn --batch-mode \
+        -Dtag=dimmer-$1 \
+        -Dproject.rel.com.github.cloudyrock.dimmer:dimmer-core2=$1 \
+        -Dproject.dev.com.github.cloudyrock.dimmer:dimmer-core2=$2-SNAPSHOT \
+        release:prepare;
+    mvn release:perform
+fi
