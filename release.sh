6b48be1b1f501bbda96696f896c0e359193fb991
diepppa <aperezdieppa@gmail.com>
2018-07-28 16:45:30 +0100

    wip

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 
-//TODO get version from pom, instead of parameter. and work out the type of release(breaking change, minor, hotfix) from branch
+#TODO get version from pom, instead of parameter. and work out the type of release(breaking change, minor, hotfix) from branch
 #$1 release version
 #$2 dev version
 if [ -z "$1" ] || [ -z "$2" ]; then
@@ -11,6 +11,8 @@ else
         -Dtag=dimmer-$1 \
         -Dproject.rel.com.github.cloudyrock.dimmer:dimmer-core2=$1 \
         -Dproject.dev.com.github.cloudyrock.dimmer:dimmer-core2=$2-SNAPSHOT \
-        release:prepare;
-    mvn release:perform
+        release:prepare
+    if [ $? -eq 0 ]; then
+        mvn release:perform
+    fi
 fi
