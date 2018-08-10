96ced58a5872d92b7e7163f0d03e13f814e06878
diepppa <aperezdieppa@gmail.com>
2018-08-10 22:50:25 +0100

    basic slf4 logging wrapper

--- dimmer-core/src/test/java/com.github.cloudyrock.dimmer/DummyFeatureProcessor.java
+++ dimmer-core/src/test/java/com.github.cloudyrock.dimmer/DummyFeatureProcessor.java
@@ -2,8 +2,6 @@ package com.github.cloudyrock.dimmer;
 
 public class DummyFeatureProcessor extends FeatureProcessorBase {
 
-
-
     public Object executeDimmerFeature(String feature,
                                        FeatureInvocation featureInvocation) {
         return runFeature(feature, featureInvocation);
