eb3cb5e4fd2fce6f43683791e58c510c3cbebadd
Dhivesh Sadhwani <dhivesh.sadhwani@gmail.com>
2018-08-18 16:46:21 +0100

    [ISSUE_63] WIP Init server config

---
+++ dimmer-server/src/main/java/com/github/cloudyrock/dimmer/dimmerserver/DimmerServerApplication.java
@@ -0,0 +1,12 @@
+package com.github.cloudyrock.dimmer.dimmerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimmerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DimmerServerApplication.class, args);
+    }
+}
