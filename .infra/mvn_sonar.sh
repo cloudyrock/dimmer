e5d31ab1b546a8a0a8523ca66ae5d6ea9feba3b7
dieppa <aperezdieppa@gmail.com>
2018-10-01 17:57:07 +0100

    test pull rquest travis

--- .infra/mvn_sonar.sh
+++ .infra/mvn_sonar.sh
@@ -3,6 +3,8 @@
 
 
 echo "\n\n\n\n---------------------------------------------------------------------------------------------------------------------Branch: $TRAVIS_BRANCH\n\n\n\n"
+echo "\n\n\n\n---------------------------------------------------------------------------------------------------------------------Pull request key: $TRAVIS_PULL_REQUEST\n\n\n\n"
+echo "\n\n\n\n---------------------------------------------------------------------------------------------------------------------Pull request branch: $TRAVIS_PULL_REQUEST_BRANCH\n\n\n\n"
 echo "\n\n\n\n---------------------------------------------------------------------------------------------------------------------Encripted variables: $TRAVIS_SECURE_ENV_VARS\n\n\n\n"
 
 
@@ -10,6 +12,6 @@ if [[ $TRAVIS_BRANCH == 'master' ]]
 then
     mvn clean verify sonar:sonar -Dsonar.projectKey=com.github.cloudyrock.dimmer -Dsonar.organization=cloudyrock -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN
 else
-    mvn clean verify sonar:sonar -Dsonar.projectKey=com.github.cloudyrock.dimmer -Dsonar.organization=cloudyrock -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=b3ad74729ae5d41a7248e50e686d44b755f3a092 -Dsonar.pullrequest.base=master -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH
+    mvn clean verify sonar:sonar -Dsonar.projectKey=com.github.cloudyrock.dimmer -Dsonar.organization=cloudyrock -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.pullrequest.base=master -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH
 fi
 
