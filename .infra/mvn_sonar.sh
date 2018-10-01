cc8e596d1b49bdb017b369d831e0c14e07feb4da
dieppa <aperezdieppa@gmail.com>
2018-10-01 19:04:05 +0100

    updated travis script

--- .infra/mvn_sonar.sh
+++ .infra/mvn_sonar.sh
@@ -8,10 +8,15 @@ echo "--------------------------------------------------------------------------
 echo "---------------------------------------------------------------------------------------------------------------------Encripted variables: $TRAVIS_SECURE_ENV_VARS"
 
 
-if [[ $TRAVIS_BRANCH == 'master' ]]
+if [[ -z $TRAVIS_PULL_REQUEST_BRANCH ]]
 then
+    echo "Verifying and running sonar for  master branch"
     mvn clean verify sonar:sonar -Dsonar.projectKey=com.github.cloudyrock.dimmer -Dsonar.organization=cloudyrock -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN
-else
+elif [[ $TRAVIS_SECURE_ENV_VARS = true ]]
+then
+    echo "Verifying and running sonar for  Pull request"
     mvn clean verify sonar:sonar -Dsonar.projectKey=com.github.cloudyrock.dimmer -Dsonar.organization=cloudyrock -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN -Dsonar.pullrequest.base=master -Dsonar.pullrequest.key=$TRAVIS_PULL_REQUEST -Dsonar.pullrequest.branch=$TRAVIS_PULL_REQUEST_BRANCH
+else
+    echo "Verifying(NO SONAR) Pull request"
+    mvn clean verify
 fi
-
