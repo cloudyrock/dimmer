80b77d57f665ce7a72f39e387f9531044a745cd1
Dhivesh Sadhwani <dhivesh.sadhwani@gmail.com>
2018-12-15 15:34:24 +0000

    [ISSUE_124] DimmerConfigReader - added more logic and test

--- dimmer-core/src/main/java/com/github/cloudyrock/dimmer/reader/DimmerConfigReaderYamlImpl.java
+++ dimmer-core/src/main/java/com/github/cloudyrock/dimmer/reader/DimmerConfigReaderYamlImpl.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.github.cloudyrock.dimmer.DimmerConfigException;
 import com.github.cloudyrock.dimmer.reader.models.yaml.DimmerYamlConfig;
-import com.github.cloudyrock.dimmer.reader.models.yaml.Environments;
+import com.github.cloudyrock.dimmer.reader.models.yaml.Environment;
 import org.slf4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 import java.util.Map;
 
 import static java.util.stream.Collectors.toMap;
@@ -18,14 +20,27 @@ import static org.slf4j.LoggerFactory.getLogger;
 final class DimmerConfigReaderYamlImpl implements DimmerConfigReader {
 
     private static Logger LOGGER = getLogger(DimmerConfigReaderYamlImpl.class);
+    private ObjectMapper objectMapper;
+
+    public static final String DIMMER_CONFIG_EXCEPTION_ENVIRONMENT_CONFIGURATION_IS_EMPTY = "Environment configuration is empty";
+    public static final String DIMMER_CONFIG_EXCEPTION_SERVER_CONFIGURATION_AND_FEATURE_INTERCEPTOR_MISMATCH = "Server Configuration and Feature Interceptor must not be configured for the same environment.";
+    public static final String DIMMER_CONFIG_EXCEPTION_INVALID_URL = "Invalid URL set in server configuration.";
     public static final String DIMMER_CONFIGURATION_FILE_COULD_NOT_BE_READ = "Dimmer configuration file could not be read.";
+
+    public DimmerConfigReaderYamlImpl() {
+        //Default to YAML object mapper
+        objectMapper = new ObjectMapper(new YAMLFactory());
+    }
+
+    public DimmerConfigReaderYamlImpl(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     @Override
     public DimmerConfig fromProperties(final String filePath) {
-        final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         try {
             final File file = readFileFromClassPath(filePath);
-            final DimmerYamlConfig dimmerYamlConfig = mapper.readValue(file, DimmerYamlConfig.class);
+            final DimmerYamlConfig dimmerYamlConfig = objectMapper.readValue(file, DimmerYamlConfig.class);
             return toDimmerConfig(dimmerYamlConfig);
         } catch (IOException e) {
             LOGGER.error("Failed mapping Dimmer configuration file.", e);
@@ -33,25 +48,53 @@ final class DimmerConfigReaderYamlImpl implements DimmerConfigReader {
         }
     }
 
-
     private DimmerConfig toDimmerConfig(DimmerYamlConfig dimmerYamlConfig) {
         final DimmerConfig dimmerConfig = new DimmerConfig();
         final Map<String, EnvironmentConfig> environmentConfigMap =
                 dimmerYamlConfig.getDimmer().getEnvironments().entrySet().stream()
-                        .collect(toMap(Map.Entry::getKey,
-                                k -> {
-                                    final Environments value = k.getValue();
-                                    return new EnvironmentConfig(
-                                            value.getServer(),
-                                            value.getFeatureIntercept(),
-                                            value.isDefault()
-                                    );
-                                }));
+                    .collect(toMap(Map.Entry::getKey,
+                        k -> {
+                            final Environment value = k.getValue();
+                            final List featuresList = value.getFeatureIntercept();
+                            final String server = value.getServer();
+                            final Boolean isDefault = value.isDefault();
+
+                            checkEnvironmentSettings(featuresList, server, isDefault);
+
+                            return new EnvironmentConfig(
+                                    value.getServer(),
+                                    value.getFeatureIntercept(),
+                                    value.isDefault()
+                            );
+                        }));
 
         dimmerConfig.setEnvironments(environmentConfigMap);
         return dimmerConfig;
     }
 
+    private void checkEnvironmentSettings(List featuresList, String server, Boolean isDefault) throws DimmerConfigException {
+
+        //server or featureIntercept don't exist
+        if ((featuresList == null) && (server == null)) {
+            throw new DimmerConfigException(DIMMER_CONFIG_EXCEPTION_ENVIRONMENT_CONFIGURATION_IS_EMPTY);
+        }
+
+        //server and featureIntercept both exist simultaneously
+        if ((featuresList != null && !featuresList.isEmpty()) && (server != null && !server.isEmpty())) {
+            throw new DimmerConfigException(DIMMER_CONFIG_EXCEPTION_SERVER_CONFIGURATION_AND_FEATURE_INTERCEPTOR_MISMATCH);
+        }
+        checkUrl(server);
+    }
+
+    private static void checkUrl(String server) {
+        if (server != null && !server.isEmpty()) {
+            try {
+                new URL(server);
+            } catch (MalformedURLException e) {
+                throw new DimmerConfigException(DIMMER_CONFIG_EXCEPTION_INVALID_URL);
+            }
+        }
+    }
 
     private File readFileFromClassPath(String filePath) {
         final URL url = getClass().getClassLoader().getResource(filePath);
---
+++ dimmer-core/src/test/java/com/github/cloudyrock/dimmer/reader/DimmerConfigReaderYamlImplUTest.java
@@ -0,0 +1,150 @@
+package com.github.cloudyrock.dimmer.reader;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.cloudyrock.dimmer.DimmerConfigException;
+import com.github.cloudyrock.dimmer.reader.models.yaml.Dimmer;
+import com.github.cloudyrock.dimmer.reader.models.yaml.DimmerYamlConfig;
+import com.github.cloudyrock.dimmer.reader.models.yaml.Environment;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import static com.github.cloudyrock.dimmer.reader.DimmerConfigReaderYamlImpl.DIMMER_CONFIGURATION_FILE_COULD_NOT_BE_READ;
+import static com.github.cloudyrock.dimmer.reader.DimmerConfigReaderYamlImpl.DIMMER_CONFIG_EXCEPTION_ENVIRONMENT_CONFIGURATION_IS_EMPTY;
+import static com.github.cloudyrock.dimmer.reader.DimmerConfigReaderYamlImpl.DIMMER_CONFIG_EXCEPTION_INVALID_URL;
+import static com.github.cloudyrock.dimmer.reader.DimmerConfigReaderYamlImpl.DIMMER_CONFIG_EXCEPTION_SERVER_CONFIGURATION_AND_FEATURE_INTERCEPTOR_MISMATCH;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DimmerConfigReaderYamlImplUTest {
+
+
+    @Test
+    public void shouldLoadDimmerConfigWithCorrectFile_happyPath() {
+
+        final DimmerConfig dimmerConfig = new DimmerConfigReaderYamlImpl().fromProperties("./dimmer.yml");
+        assertNotNull(dimmerConfig);
+        assertNotNull(dimmerConfig.getEnvironments());
+        assertThat(dimmerConfig.getEnvironments().size(), is(3));
+        assertThat(dimmerConfig.getEnvironments().get("dev").getFeatureIntercept().size(), is(6));
+        assertThat(dimmerConfig.getEnvironments().get("dev").isDefault(), is(true));
+    }
+
+    @Test
+    public void shouldThrowDimmerConfigExceptionWhenFileDoesntExist() {
+        try {
+            new DimmerConfigReaderYamlImpl().fromProperties("./random.yml");
+            assertTrue(false);
+        } catch (DimmerConfigException e) {
+            assertThat(e.getMessage(), is(DIMMER_CONFIGURATION_FILE_COULD_NOT_BE_READ));
+        }
+    }
+
+    @Test
+    public void shouldThrowDimmerConfigExceptionWhenServerAndFeaturesAreSetForEnvironment() throws Exception {
+
+        final ObjectMapper mapperMock = mock(ObjectMapper.class);
+        when(mapperMock.readValue(any(File.class), any(DimmerYamlConfig.class.getClass()))).thenReturn(invalidConfigServerAndFeaturesSetUpForEnv());
+
+        try {
+            final DimmerConfig dimmerConfig = new DimmerConfigReaderYamlImpl(mapperMock).fromProperties("./dimmer.yml");
+            assertThat(dimmerConfig.getEnvironments().size(), is(1));
+
+        } catch (DimmerConfigException e) {
+            assertThat(e.getMessage(), is(DIMMER_CONFIG_EXCEPTION_SERVER_CONFIGURATION_AND_FEATURE_INTERCEPTOR_MISMATCH));
+        }
+        verify(mapperMock, times(1)).readValue(any(File.class), any(DimmerYamlConfig.class.getClass()));
+    }
+
+    @Test
+    public void shouldThrowDimmerConfigExceptionWhenServerIsNotURL() throws Exception {
+
+        final ObjectMapper mapperMock = mock(ObjectMapper.class);
+        when(mapperMock.readValue(any(File.class), any(DimmerYamlConfig.class.getClass()))).thenReturn(invalidConfigWrongServerFormat());
+
+        try {
+            final DimmerConfig dimmerConfig = new DimmerConfigReaderYamlImpl(mapperMock).fromProperties("./dimmer.yml");
+            assertThat(dimmerConfig.getEnvironments().size(), is(1));
+
+        } catch (DimmerConfigException e) {
+            assertThat(e.getMessage(), is(DIMMER_CONFIG_EXCEPTION_INVALID_URL));
+        }
+        verify(mapperMock, times(1)).readValue(any(File.class), any(DimmerYamlConfig.class.getClass()));
+    }
+
+    @Test
+    public void shouldThrowDimmerConfigExceptionWhenServerAndFeaturesEmptyForEnv() throws Exception {
+
+        final ObjectMapper mapperMock = mock(ObjectMapper.class);
+        when(mapperMock.readValue(any(File.class), any(DimmerYamlConfig.class.getClass()))).thenReturn(invalidConfigEmptyEnv());
+
+        try {
+            final DimmerConfig dimmerConfig = new DimmerConfigReaderYamlImpl(mapperMock).fromProperties("./dimmer.yml");
+            assertThat(dimmerConfig.getEnvironments().size(), is(1));
+
+        } catch (DimmerConfigException e) {
+            assertThat(e.getMessage(), is(DIMMER_CONFIG_EXCEPTION_ENVIRONMENT_CONFIGURATION_IS_EMPTY));
+        }
+        verify(mapperMock, times(1)).readValue(any(File.class), any(DimmerYamlConfig.class.getClass()));
+    }
+
+    private static DimmerYamlConfig invalidConfigServerAndFeaturesSetUpForEnv() {
+
+        final Dimmer dimmer = new Dimmer();
+        final HashMap<String, Environment> environments = new HashMap<>();
+        final Environment env1 = new Environment();
+        env1.setServer("ASDF");
+        env1.setFeatureIntercept(new ArrayList<>(Arrays.asList("feature1", "feature2", "feature3")));
+        environments.put("env1", env1);
+        dimmer.setEnvironments(environments);
+
+        final DimmerYamlConfig dimmerYamlConfig = new DimmerYamlConfig();
+        dimmerYamlConfig.setDimmer(dimmer);
+
+        return dimmerYamlConfig;
+    }
+
+    private static DimmerYamlConfig invalidConfigWrongServerFormat() {
+
+        final Dimmer dimmer = new Dimmer();
+        final HashMap<String, Environment> environments = new HashMap<>();
+        final Environment env1 = new Environment();
+        env1.setServer("RANDOM");
+        environments.put("env1", env1);
+        dimmer.setEnvironments(environments);
+
+        final DimmerYamlConfig dimmerYamlConfig = new DimmerYamlConfig();
+        dimmerYamlConfig.setDimmer(dimmer);
+
+        return dimmerYamlConfig;
+    }
+
+    private static DimmerYamlConfig invalidConfigEmptyEnv() {
+
+        final Dimmer dimmer = new Dimmer();
+        final HashMap<String, Environment> environments = new HashMap<>();
+        final Environment env1 = new Environment();
+        environments.put("env1", env1);
+        dimmer.setEnvironments(environments);
+
+        final DimmerYamlConfig dimmerYamlConfig = new DimmerYamlConfig();
+        dimmerYamlConfig.setDimmer(dimmer);
+
+        return dimmerYamlConfig;
+    }
+
+}
